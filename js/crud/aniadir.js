546bdee18cc16f6ba95b9b94d09f1a120269fc6f
Jose <jose.s.contacto@gmail.com>
2022-11-18 20:39:15 -0300

    Add-aniadir-logic-7

--- js/crud/aniadir.js
+++ js/crud/aniadir.js
@@ -1,1 +1,16 @@
 // este script añade un elemento al localstorage según el objeto que sea
+
+function getEntity(entity){
+    return JSON.parse(localStorage.getItem(entity));
+}
+
+function add(entity,record){
+    getEntity(entity).list.push(record);
+}
+
+
+//including crud files
+
+import * as aniadir from 'aniadir.js';
+import * as eliminar from 'eliminar.js';
+import * as actualizar from 'actualizar.js';
